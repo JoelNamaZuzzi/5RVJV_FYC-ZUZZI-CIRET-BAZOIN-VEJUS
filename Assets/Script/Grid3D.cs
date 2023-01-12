d66fca9641705674fdf9d0312892a59723761675
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2023-01-12 17:23:51 +0100

    Changed bubulles generation

--- Assets/Script/Grid3D.cs
+++ Assets/Script/Grid3D.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -12,16 +13,16 @@ public class Grid3D : MonoBehaviour
     public float[,,] density;
     public float[,,] pressure;
     public GameObject bubullePrefab;
-    public GameObject[,,] particleGrid;
+    public int nbBubulle;
+    public GameObject[] bubulles;
 
-    void Start()
+    void Awake()
     {
         //Initialize grid
         cell_size = new Vector3(grid_size.x / cells_x, grid_size.y / cells_y, grid_size.z / cells_z);
         velocity = new Vector3[cells_x, cells_y, cells_z];
         density = new float[cells_x, cells_y, cells_z];
         pressure = new float[cells_x, cells_y, cells_z];
-        particleGrid = new GameObject[cells_x,cells_y,cells_z];
         // Initialize grid cells with default values
         for (int i = 0; i < cells_x; i++) {
             for (int j = 0; j < cells_y; j++) {
@@ -30,31 +31,36 @@ public class Grid3D : MonoBehaviour
                     velocity[i, j, k] = Vector3.zero;
                     density[i, j, k] = 0.0f;
                     pressure[i, j, k] = 0.0f;
-                    //Initialize bubulle
-                    Vector3 pos = Vector3.Scale(new Vector3(i,j,k), cell_size);
-                    GameObject bubulle = Instantiate(bubullePrefab, pos, Quaternion.identity);
-                    bubulle.transform.parent = transform;
-                    particleGrid[i, j, k] = bubulle;
                 }
             }
         }
-    } 
+
+        bubulles = new GameObject[nbBubulle];
+        for(int i=0; i<nbBubulle; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(0, grid_size.x * cells_x+transform.position.x), Random.Range(0, grid_size.y * cells_y+transform.position.y),
+                Random.Range(0, grid_size.z * cells_z+transform.position.z));
+            GameObject bubulle = Instantiate(bubullePrefab, pos, Quaternion.identity);
+            bubulles.Append(bubulle);
+            bubulle.transform.parent = transform;
+        }
+    }
     
     //Updating the cells & particles
     void UpdateFluid(Grid3D grid, float dt) {
         // Step 1: Advection
-        Vector3[,,] new_velocity = new Vector3[grid.cells_x, grid.cells_y, grid.cells_z];
-        for (int i = 0; i < grid.cells_x; i++) {
-            for (int j = 0; j < grid.cells_y; j++) {
-                for (int k = 0; k < grid.cells_z; k++)
+        Vector3[,,] new_velocity = new Vector3[cells_x, cells_y, cells_z];
+        for (int i = 0; i < cells_x; i++) {
+            for (int j = 0; j < cells_y; j++) {
+                for (int k = 0; k < cells_z; k++)
                 {   
                     // Calculate new position of particle
-                    //Vector3 pos = new Vector3(i, j, k) * grid.cell_size;
-                    Vector3 pos = Vector3.Scale(new Vector3(i, j, k), grid.cell_size);
-                    Vector3 new_pos = pos - grid.velocity[i, j, k] * dt;
+                    //Vector3 pos = new Vector3(i, j, k) * cell_size;
+                    Vector3 pos = Vector3.Scale(new Vector3(i, j, k), cell_size);
+                    Vector3 new_pos = pos - velocity[i, j, k] * dt;
 
                     // Use trilinear interpolation to estimate new velocity
-                    //new_velocity[i, j, k] = TrilinearInterpolation(grid.velocity, new_pos, grid.grid_size, grid.cells_x, grid.cells_y, grid.cells_z);
+                    //new_velocity[i, j, k] = TrilinearInterpolation(velocity, new_pos, grid_size, cells_x, cells_y, cells_z);
                 }
             }
         }
@@ -65,38 +71,38 @@ public class Grid3D : MonoBehaviour
         // Calculate new velocity
         // ...
         // Calculate divergence of velocity field
-        float[,,] divergence = new float[grid.cells_x, grid.cells_y, grid.cells_z];
-        for (int i = 1; i < grid.cells_x-1; i++) {
-            for (int j = 1; j < grid.cells_y-1; j++) {
-                for (int k = 1; k < grid.cells_z-1; k++)
+        float[,,] divergence = new float[cells_x, cells_y, cells_z];
+        for (int i = 1; i < cells_x-1; i++) {
+            for (int j = 1; j < cells_y-1; j++) {
+                for (int k = 1; k < cells_z-1; k++)
                 {
-                    divergence[i, j, k] = (grid.velocity[i+1, j, k].x - grid.velocity[i-1, j, k].x)/(2 * grid.cell_size.x)
-                                          + (grid.velocity[i, j+1, k].y - grid.velocity[i, j-1, k].y)/(2 * grid.cell_size.y)
-                                          + (grid.velocity[i, j, k+1].z - grid.velocity[i, j, k-1].z)/(2 * grid.cell_size.z);
+                    divergence[i, j, k] = (velocity[i+1, j, k].x - velocity[i-1, j, k].x)/(2 * cell_size.x)
+                                          + (velocity[i, j+1, k].y - velocity[i, j-1, k].y)/(2 * cell_size.y)
+                                          + (velocity[i, j, k+1].z - velocity[i, j, k-1].z)/(2 * cell_size.z);
                 }
             }
         }
 
         // Solve Poisson equation to calculate pressure
-        float[,,] pressure = new float[grid.cells_x, grid.cells_y, grid.cells_z];
+        float[,,] pressure = new float[cells_x, cells_y, cells_z];
         // Perform Jacobi iterations to solve Poisson equation
         // and calculate pressure
 
         // Correct velocity field using pressure
-        for (int i = 1; i < grid.cells_x-1; i++) {
-            for (int j = 1; j < grid.cells_y-1; j++) {
-                for (int k = 1; k < grid.cells_z-1; k++)
+        for (int i = 1; i < cells_x-1; i++) {
+            for (int j = 1; j < cells_y-1; j++) {
+                for (int k = 1; k < cells_z-1; k++)
                 {
-                    grid.velocity[i, j, k].x -= (pressure[i+1, j, k] - pressure[i-1, j, k]) * dt / grid.cell_size.x;
-                    grid.velocity[i, j, k].y -= (pressure[i, j+1, k] - pressure[i, j-1, k]) * dt / grid.cell_size.y;
-                    grid.velocity[i, j, k].z -= (pressure[i, j, k+1] - pressure[i, j, k-1]) * dt / grid.cell_size.z;
+                    velocity[i, j, k].x -= (pressure[i+1, j, k] - pressure[i-1, j, k]) * dt / cell_size.x;
+                    velocity[i, j, k].y -= (pressure[i, j+1, k] - pressure[i, j-1, k]) * dt / cell_size.y;
+                    velocity[i, j, k].z -= (pressure[i, j, k+1] - pressure[i, j, k-1]) * dt / cell_size.z;
                 }
             }
         }
         // Update grid with new properties
-        grid.velocity = new_velocity;
-        // grid.density = new_density;
-        // grid.pressure = new_pressure;
+        velocity = new_velocity;
+        // density = new_density;
+        // pressure = new_pressure;
     }
 }
 
