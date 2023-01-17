8111774250cea41804537930037e3e9761c423f3
rvejus <76257185+rvejus@users.noreply.github.com>
2023-01-17 11:22:35 +0100

    Mise a jour density + rajout container

--- Assets/Script/Bubulle.cs
+++ Assets/Script/Bubulle.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bubulle : MonoBehaviour
@@ -10,12 +11,15 @@ public class Bubulle : MonoBehaviour
     public Vector3 force;       // vitesse de la particule
     public float density;       // densité de la particule
     public float pressure;      // pression de la particule
+    public float volume;
     [HideInInspector]public Rigidbody rigidbody;
 
     private void Awake()
     {
         rigidbody = this.GetComponent<Rigidbody>();
         rigidbody.mass = 1.0f;
+        volume = 4 / 3 * Mathf.PI * Mathf.Pow(GetComponent<SphereCollider>().radius, 3);
+        density = rigidbody.mass / volume;
     }
 
     public Bubulle(Vector3 pos)
